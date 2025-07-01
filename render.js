7be99967a7151ea7db626faf627015c4961e93ff
Luma <nonocut@icloud.com>
2025-07-01 01:19:38 -0400

    sha256, multiple ir

--- render.js
+++ render.js
@@ -5,13 +5,10 @@
     const resHeader = await fetch(headerURL);
     const header = await resHeader.json();
 
-    // Get base path from head.json URL
     const baseURL = new URL(headerURL, window.location.href);
     const basePath = baseURL.href.substring(0, baseURL.href.lastIndexOf('/') + 1);
 
-    // Resolve data_url relative to head.json's location
     const dataURL = new URL(header.data_url, basePath);
-
     const resData = await fetch(dataURL);
     const charts = await resData.json();
 
@@ -35,7 +32,7 @@
         // Section header row
         const rowHead = document.createElement("tr");
         rowHead.className = "level-header";
-        rowHead.innerHTML = `<th colspan="7">(=^･ω･^)${level} <span style="font-weight: normal;">(${chartsAtLevel.length} songs)</span></th>`;
+        rowHead.innerHTML = `<th colspan="8">(=^･ω･^)${level} <span style="font-weight: normal;">(${chartsAtLevel.length} songs)</span></th>`;
         tbody.appendChild(rowHead);
 
         // Column titles
@@ -44,6 +41,7 @@
         colHeader.innerHTML = `
           <th>LV</th>
           <th>♪</th>
+          <th>IR</th>
           <th>Title</th>
           <th>Artist</th>
           <th>Genre</th>          
@@ -58,10 +56,24 @@
             .forEach((chart, i) => {
                 const row = document.createElement("tr");
                 row.style.backgroundColor = i % 2 === 0 ? "#ffe0e0" : "#eebdbd";
+
+                const lr2irURL = `http://www.dream-pro.info/~lavalse/LR2IR/search.cgi?mode=ranking&bmsmd5=${chart.md5}`;
+                const mochaURL = chart.sha256
+                    ? `https://mocha-repository.info/song.php?sha256=${chart.sha256}`
+                    : null;
+                const minirURL = chart.sha256
+                    ? `https://www.gaftalk.com/minir/#/viewer/song/${chart.sha256}/0`
+                    : null;
+
+                const irLinks = [`<a href="${lr2irURL}" target="_blank">LR2</a>`];
+                if (mochaURL) irLinks.push(`<a href="${mochaURL}" target="_blank">Mocha</a>`);
+                if (minirURL) irLinks.push(`<a href="${minirURL}" target="_blank">MinIR</a>`);
+
                 row.innerHTML = `
                   <td>${chart.level}</td>
                   <td><a href="https://bms-score-viewer.pages.dev/view?md5=${chart.md5}" target="_blank">♪</a></td>
-                  <td><a href="http://www.dream-pro.info/~lavalse/LR2IR/search.cgi?mode=ranking&bmsmd5=${chart.md5}" target="_blank">${chart.title}</a></td>
+                  <td>${irLinks.join(" ")}</td>
+                  <td>${chart.title || ''}</td>
                   <td>${chart.artist || ''}</td>
                   <td>${chart.genre || ''}</td>
                   <td>${chart.bpm || ''}</td>
